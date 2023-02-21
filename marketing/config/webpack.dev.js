c921f47fa7a5b93a6969e1126105c2cb1431858a
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2023-02-22 00:09:50 +0300

    Delegating Shared Module Selection

--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -2,6 +2,7 @@ const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
+const packageJson = require('../package.json');
 const commonConfig = require('./webpack.common');
 
 const devConfig = {
@@ -19,7 +20,7 @@ const devConfig = {
       exposes: {
         './MarketingApp': './src/bootstrap',
       },
-      shared: ['react', 'react-dom'],
+      shared: packageJson.dependencies,
     }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
